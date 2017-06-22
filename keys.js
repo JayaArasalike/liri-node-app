e62f2baecbb225394eda9c4945aa2734f2a6bebc
Jayalaxmi Arasalike <jayalaxmiarasalike@Jayalaxmis-MacBook-Pro.local>
2017-06-22 00:00:25 -0700

    fully functioning program

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  	consumer_key: 'lKCeoyjQ1kdpUtWm77HhagTtd'
+  , consumer_secret: 'WA54FRYj5aXIypcW3fnllKhummk1e9O9Ub84ba5Bq3uK95hpQo'
+  ,	access_token_key: '876313798696550400-S9H62tkNcNCq7wmUSrpPaYS8dc8OqXp'
+  , access_token_secret: 'JNu0Bf1J4SjXZE08B3yR8TFF6jCWC5CuDfE12EUwcIxPI'	
+}
+
+exports.spotifyKeys = {
+    id: 'd7371510c05541d6a22c178441f87827'
+  , secret: '8957856164a9447cb9bcb4412c951ec6'
+}
---
+++ liri.js
@@ -0,0 +1,206 @@
+
+
+var keys = require('./keys');
+var twitter = require('Twitter');
+var Spotify = require('node-spotify-api');
+var request = require("request");
+var fs = require("fs");
+var colors = require('colors');
+
+console.log("liri is starting...".rainbow);
+//console.log("The bot is starting...");
+//console.log("Keys: ", keys);
+
+var T = new twitter(keys.twitterKeys);
+var spotify = new Spotify(keys.spotifyKeys);
+
+var value = process.argv[2];
+var query1 = process.argv[3];
+var movieName2 = [];
+
+//=======================================================================================
+//Writing to a file 
+function myLog(message='', log2Console=true) {
+	if (log2Console) {
+		console.log(message);
+	}
+	//write to a file
+	message = message + '\n';
+	fs.appendFile('log.txt', message, function(err) {
+		if (err) {
+    		console.log(err);
+  		}
+	});
+
+}
+
+// myLog();
+// myLog('dskjfhaskjf');
+// myLog('asdfas', false);
+
+
+//=======================================================================================
+
+function liriInfo() {
+	//myLog("Testing......");
+	console.log("========================================================================")
+	console.log("LIRI is a Language Interpretation and Recognition Interface. ".bold + 
+		"LIRI is a command line node app that takes in parameters and gives you back data.".bold);
+	console.log("------------------------------------------------------------------------");
+	console.log("liri.js can take the following commands:".italic.black.bgWhite);
+	console.log("------------------------------------------------------------------------");
+	console.log('my-tweets'.green.bold,  "displays your latest tweets");
+	console.log('spotify-this-song'.green.bold, "followed by the song name displays track/artist/album");
+	console.log('movie-this'.green.bold, "followed by movie name displays movie details...like year released, actors, plot etc...");
+	console.log('do-what-it-says'.green.bold, "reads file and executes the command to give results," + 
+		"specifically info about song 'I like it this way'");
+	console.log("========================================================================")
+}
+if(!value)
+liriInfo();
+
+
+//=======================================================================================
+
+function myTweets() {
+
+	var params = {
+		q: 'jsansk',
+		count: 5
+	};
+
+	T.get('search/tweets', params, gotData);
+
+	function gotData(error, data, response) {
+		//console.log(data);
+		var tweets = data.statuses;
+		console.log("Your recent tweets are:".bgWhite.red);
+		myLog('my-tweets', false);
+		for (var i=0; i<tweets.length; i++)
+		{
+			//console.log(tweets[i].text + ' published on '.magenta + tweets[i].created_at);
+			myLog(tweets[i].text + ' published on '.magenta + tweets[i].created_at);
+  		}
+	}
+} //end of function
+
+//at the commandline, when user gives 3rd argument 'my-tweets'
+if(value === 'my-tweets') 
+	myTweets();
+else
+	console.log("You did not want tweets!!".america);
+
+
+//=======================================================================================
+var displaySpotifyResults = function (err, data){
+  		if (err) {
+    		return console.log('Error occurred: ' + err);
+  		}
+ 		else {
+ 			var temp = 'spotify-this-song ' + query1;
+ 			myLog(temp, false);
+ 			var songInfo = data.tracks.items;
+ 			for (var i = 0; i< data.tracks.items.length; i++) {
+	    		mylog(">>>>>Artist: ".bold.blue, songInfo[i].artists[0].name)
+	            console.log("Song Name: ".bold.blue, songInfo[i].name)
+	            console.log("Album Name: ".bold.blue, songInfo[i].album.name)
+	            console.log("Preview Link:".bold.blue, songInfo[i].preview_url) 
+	            console.log("===========================================================")
+        	} //end-of-for loop
+ 		} //end of else part
+	}; //end of spotify search function
+
+
+
+function spotifyThisSong(){
+	
+ 	//If no song is provided, "The Sign" by Ace of Base is chosen as deafult
+ 	if(query1) {
+ 		
+ 		spotify.search({type: 'track', query: query1}, displaySpotifyResults);
+ 	}
+ 	else {
+ 			query1 = 'The Sign'; 
+ 			spotify.search({type:'track', query: query1} ,displaySpotifyResults);	
+ 	}
+
+} // end of function
+
+//at the commandline, when user gives 3rd argument 
+if(value === 'spotify-this-song') 
+	spotifyThisSong();
+else
+	console.log("You dint request for song detials...".america);
+
+//=======================================================================================
+//OMDB API
+
+function movieThis(){
+	if(process.argv.length > 3){
+		for(var i = 3; i< process.argv.length; i++)
+			movieName2 += process.argv[i]+'+';
+		}
+	else
+		movieName2 = 'Mr. Nobody';
+
+	//console.log("Movie Name: ", movieName2);
+	// Then run a request to the OMDB API with the movie specified
+	var queryUrl = "http://www.omdbapi.com/?t=" + movieName2 + "&y=&plot=short&apikey=40e9cece";
+	request(queryUrl, function(error, response, body){
+
+		if (!error && response.statusCode === 200) {
+
+    	// Parse the body of the site and recovering the results
+    		console.log("==========================================================");
+	    	console.log("The movie title is:".bold.underline + JSON.parse(body).Title);
+		    console.log("The movie's release date is:".bold.underline + JSON.parse(body).Released);
+		    console.log("IMDB Rating is:".bold.underline + JSON.parse(body).imdbRating);
+		    console.log("Country[ies]:".bold.underline+ JSON.parse(body).Country);
+		    console.log("The movie Language is:".bold.underline + JSON.parse(body).Language);
+		    console.log("The movie plot is:".bold.underline+ JSON.parse(body).Plot);
+		    console.log("The movie actors are:".bold.underline+ JSON.parse(body).Actors);
+		    console.log("===========================================================");
+  	}
+}); //end of request
+
+}
+
+if(value === 'movie-this')
+	movieThis();
+else 
+	console.log("You dint ask for a movie...Maybe next time!".america);
+
+//============================================================================================
+//reading from file system
+
+function doWhatItSays(){
+	var fs = require("fs");
+	fs.readFile("random.txt", "utf-8", function(error, data){
+		if (error)
+			return console.log(error);
+	
+		//console.log(data);
+		var dataArr = data.split(",");
+		//console.log(dataArr);
+
+		// for(var i=0; i<dataArr.length; i++) {
+		// 	//console.log(dataArr[i]);
+		// 	process.argv[3] = dataArr[i];
+		// 	process.argv[4] = dataArr[++i];
+		// }
+		if(dataArr[0] === 'spotify-this-song') {
+			query1 = dataArr[1];
+			spotifyThisSong();
+		}
+
+	}); //end of read file
+}
+
+//if user gives 3rd commandline argument as 'do-what-it-says'
+if(value === 'do-what-it-says')
+	doWhatItSays();
+else
+	console.log("You did not request 'do-what-it-says'".america);
+
+//=======================================================================================
+
